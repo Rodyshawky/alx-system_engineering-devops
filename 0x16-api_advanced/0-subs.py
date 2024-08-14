b1aee88f6380354fc4748ec9ab20f64e37fea387
rodyshawky <rodashawkei@gmail.com>
2024-08-14 12:24:35 -0700

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """Module for task 0"""
 
@@ -16,4 +15,3 @@ def number_of_subscribers(subreddit):
         return 0
 
     return sub_info.json().get("data").get("subscribers")
-
